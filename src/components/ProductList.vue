7fc45033887ade834ad32234e8c718744700592d
Omar Coronado <omar.coronado@gaiadesign.com.mx>
2020-04-23 20:24:40 -0500

    products module

---
+++ src/components/ProductList.vue
@@ -0,0 +1,53 @@
+<template>
+    <div v-if="products.length">
+        <paginate name="products" :list="products" :per="perPage">
+            <b-card-group columns>
+                <product-item
+                        v-for="product in paginated('products')"
+                        :product="product"
+                        :key="product.id"
+                        @addToCart="addProductToCart"
+                >
+
+                </product-item>
+            </b-card-group>
+        </paginate>
+        <paginate-links for="products" :limit="5" :show-step-links="true"
+                        :classes="{'ul': 'pagination', 'li': 'page-item', 'li > a': 'page-link'}"></paginate-links>
+    </div>
+    <b-alert v-else show variant="info">No hay productos que mostrar</b-alert>
+</template>
+
+<script>
+    import {mapActions, mapMutations, mapState} from 'vuex'
+    import ProductItem from "../store/ProductItem";
+    export default {
+        name: "ProductList",
+        components: {
+          ProductItem
+        },
+        mounted() {
+            this.fetchProducts()
+        },
+        data() {
+            return {
+                paginate: ['products'],
+                perPage: 3
+            }
+        },
+        computed: {
+            ...mapState('products', ['products'])
+        },
+        methods: {
+            ...mapActions('products', ['fetchProducts']),
+
+            addProductToCart(producto) {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/store/ProductItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <b-card
+        :title="product.name"
+        :img-src="product.picture"
+        :img-alt="product.name"
+        img-top
+        style="max-width: 30rem;"
+        class="mb-2"
+    >
+        <b-button block variant="warning" @click="$emit('addToCart', product)">
+            Añadir al Carrito
+        </b-button>
+    </b-card>
+</template>
+
+<script>
+    export default {
+        name: "ProductItem",
+        props: {
+            product: {
+                type: Object,
+                required: true
+            }
+        }
+    }
+</script>
