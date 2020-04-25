50578db8a0e41f7719ff79e1b386a2f4de7babaf
Omar Coronado <omar.coronado@gaiadesign.com.mx>
2020-04-25 17:33:33 -0500

    cart persistent

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import products from '../modules/products'
+import cart from '../modules/Cart'
+import VuexPersistence from "vuex-persist";
+import localforage from "localforage";
 
 Vue.use(Vuex)
 
-import products from '../modules/products'
-import cart from '../modules/Cart'
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+  modules: ['cart']
+})
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
     products,
     cart
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
